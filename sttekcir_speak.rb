8eb5397c3e39eb1c2176a290065c7731b85ea512
Bruce Ricketts <klmit1@me.com>
2011-03-24 09:25:36 -0400

    sttekcir_speak.rb now has class system

--- sttekcir_speak.rb
+++ sttekcir_speak.rb
@@ -2,11 +2,12 @@ load 'secret_decoder.rb'
 load 'secret_coder.rb'
 require 'highline/import'
 
-if ask("Welcome to SttekcirSpeak.  Enter password: ") {|q| q.echo = "*"} == "jacklikesemma"
-  while true
-    puts("type 'decode' to decode a message written in SttekcirSpeak")
-    puts("type 'encode' to encode a message written in English")
-    
+class SttekcirSpeak
+  def self.welcome_with(password)
+    return ask("Welcome to SttekcirSpeak.  Enter password: ") {|q| q.echo = "*"} == password
+  end
+  
+  def self.ask_for_input
     tips_array = [
       "You cannot use `, ~, < or > in your messages",
       "A version is secretly hidden within each encoded message, so you don't have to enter your own enter one before decoding!",
@@ -15,40 +16,51 @@ if ask("Welcome to SttekcirSpeak.  Enter password: ") {|q| q.echo = "*"} == "jac
     
     helpful_tip = tips_array[rand(tips_array.count)]
     
+    puts("type 'decode' to decode a message written in SttekcirSpeak")
+    puts("type 'encode' to encode a message written in English")
     puts("type 'quit' to exit")
-    puts("Helpful Tip: #{helpful_tip}")
+    puts("Helpful Tip: #{helpful_tip}")   
+    return ask(">> ")
+  end
+  
+  def self.decode
+    puts("You have chosen to decode")
+    puts("You may now paste an encoded message:")
+    decoder = Decoder.new
+    puts("\n")
+    input = ask("  input: ")
+    hash = decoder.decode(input)
+    puts(" output: #{hash[:output]}")
+    puts("version: #{hash[:version]}")
+    puts(" errors: #{hash[:errors]}")
+    puts("\n")
+    puts("\n")
+  end
+  
+  def self.encode
+    puts("You have chosen to encode")
+    version = ask("Which version do you prefer to encode in (latest version is #{Versioned.support.last}): ")
+    encoder = Encoder.new(version)
+    puts("You may now type a message to encode:")
+    puts("\n")
+    input = ask(" input: ")
+    hash = encoder.encode(input)
+    puts("output: #{hash[:output]}")
+    puts("errors: #{hash[:error_message]}#{hash[:errors]}")
+    puts("\n")
+    puts("\n")
+  end
+end
 
-    input = gets.chomp
-
-    if input == "decode"
-      puts("You have chosen to decode")
-      puts("You may now paste an encoded message:")
-      decoder = Decoder.new
-      puts("\n")
-      input = ask("  input: ")
-      hash = decoder.decode(input)
-      puts(" output: #{hash[:output]}")
-      puts("version: #{hash[:version]}")
-      puts(" errors: #{hash[:errors]}")
-      puts("\n")
-      puts("\n")
-    elsif input == "encode"
-      puts("You have chosen to encode")
-      version = ask("Which version do you prefer to encode in (latest version is #{Versioned.support.last}): ")
-      encoder = Encoder.new(version)
-      puts("You may now type a message to encode:")
-      puts("\n")
-      input = ask(" input: ")
-      hash = encoder.encode(input)
-      puts("output: #{hash[:output]}")
-      puts("errors: #{hash[:error_message]}#{hash[:errors]}")
-      puts("\n")
-      puts("\n")
-    elsif input == "quit"
+if SttekcirSpeak.welcome_with("")
+  while true
+    message = SttekcirSpeak.ask_for_input
+    if message == 'decode'
+      SttekcirSpeak.decode
+    elsif message == 'encode'
+      SttekcirSpeak.encode
+    elsif message == 'quit'
       break
-    else
-      puts("\n")
-      puts("Um...didn't get that.")
     end
   end
 else
